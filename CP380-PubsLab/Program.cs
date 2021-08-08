4f3c2a55093e2c7bf189d783caeffea4fb065cbc
Rohan0310 <rpalkar@confederationcollege.ca>
2021-08-08 02:37:29 -0400

    Step 1 pubsLab completed

--- CP380-PubsLab/Program.cs
+++ CP380-PubsLab/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using CP380_PubsLab.Models;
 
 namespace CP380_PubsLab
 {
@@ -13,14 +18,27 @@ namespace CP380_PubsLab
                     Console.WriteLine("Yes, I can connect");
                 }
 
+
                 // 1:Many practice
                 //
                 // TODO: - Loop through each employee
                 //       - For each employee, list their job description (job_desc, in the jobs table)
 
+                var q = dbcontext.Employee;
+                Console.WriteLine($"{q.ToQueryString()}");
+                foreach (var e in q.Include(e => e.Job))
+                {
+                    Console.WriteLine($"{e.Emp_id} -> {e.Job.Desc}");
+                }
+
                 // TODO: - Loop through all of the jobs
                 //       - For each job, list the employees (first name, last name) that have that job
-
+                foreach (var a in dbcontext.Jobs.Include(d => d.Employee))
+                {
+                    var employeesjob = a.Employee.Select(d => new { Emp_id = d.Emp_id, Fname = d.Firstname, Lname = d.Lastname, Job_Desc = a.Desc }).ToList();
+                    var empl_Job = String.Join(",", employeesjob);
+                    Console.WriteLine($"{a.Desc} -> {empl_Job}");
+                }
 
                 // Many:many practice
                 //
@@ -29,13 +47,37 @@ namespace CP380_PubsLab
                 //
                 // e.g.
                 //  Bookbeat -> The Gourmet Microwave, The Busy Executive's Database Guide, Cooking with Computers: Surreptitious Balance Sheets, But Is It User Friendly?
-                
+
+                var storeTitles = dbcontext.Stores.Select
+                    (
+                    s => new { Store = s.stor_name, Titles = s.Sales.Select(sl => sl.Title.title) }).ToList();
+
+                Console.WriteLine("\n#Stores | Titles ");
+                foreach (var store in storeTitles)
+                {
+                    var strJoin = String.Join(",", store.Titles);
+                    Console.WriteLine($"\n{store.Store} -. {strJoin}");
+                }
+
+
                 // TODO: - Loop through each Title
                 //       - For each title, list all the stores it was sold at
                 //
                 // e.g.
                 //  The Gourmet Microwave -> Doc-U-Mat: Quality Laundry and Books, Bookbeat
+
+                var Titlestore = dbcontext.Titles.Select(t => new { TitleName = t.title, StoreList = t.Sales.Select(sl => sl.Store.stor_name) }).ToList();
+
+                Console.WriteLine("\n#Titles | Stores ");
+                foreach (var title in Titlestore)
+                {
+                    var strJoin = String.Join(",", title.StoreList);
+                    Console.WriteLine($"\n{title.TitleName} -. {strJoin}");
+                }
+
             }
         }
-    }
-}
+            }
+        }
+    
+
